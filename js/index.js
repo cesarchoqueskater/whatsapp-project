fcaa9a7493401c8528aa49999a51585e8e2f1bd1
cesarchoqueskater <cesarchoqueskater@gmail.com>
2022-01-27 23:33:34 -0500

    Container-chat-contact-list hover y aria-selected

--- js/index.js
+++ js/index.js
@@ -1,6 +1,20 @@
 const layout = document.querySelector('.layout')
 const layoutSidebar = document.querySelector('.layout-sidebar')
 const layoutMain = document.querySelector('.layout-main')
+const selectContactChat = document.querySelectorAll('.container-contact-list .container-chat-contact-list')
 
 
-console.log("Ingreso")
+selectContactChat.forEach((index) => {
+    index.addEventListener('click', function() {
+        removeSelectedContactChat()
+        index.setAttribute('aria-selected', true)
+    })
+})
+
+
+export function removeSelectedContactChat() {
+    selectContactChat.forEach((index) => {
+        console.log("Eliminando")
+        index.removeAttribute('aria-selected', true)
+    })
+}
