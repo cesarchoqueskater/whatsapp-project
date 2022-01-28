16491e499c1d1c0ceb28b8a8033ed8ec4ddf05e6
cesarchoqueskater <cesarchoqueskater@gmail.com>
2022-01-27 22:55:40 -0500

    Se agrega scrollbar y logica en javascript

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const layout = document.querySelector('.layout')
+const layoutSidebar = document.querySelector('.layout-sidebar')
+const layoutMain = document.querySelector('.layout-main')
+
+
+console.log("Ingreso")
